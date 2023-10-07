8aee39f76bc2d58220067537c72337f11d878365
Ali <alihuseyni523@gmail.com>
2023-10-07 22:40:25 +0300

    Icon and background

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("com.android.volley:volley:1.2.1")
+    implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -4,13 +4,21 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -18,47 +26,92 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.weatherforecast.ui.theme.BlueLight
 import org.json.JSONObject
 
 const val API_KEY = ""
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Penza", this)
+            HomeScreen(context = this)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, context: Context) {
+fun HomeScreen(context: Context) {
+    Image(
+        painter = painterResource(id = R.drawable.weather),
+        contentDescription = "im1",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.5f),
+        contentScale = ContentScale.FillBounds
+    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(BlueLight),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "7 Okt 2023 21:00",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/night/113.png",
+                        contentDescription = "im2",
+                        modifier = Modifier.size(35.dp)
+                    )
+                }
+                Greeting("Penza", context)
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, context: Context){
     val state = remember {
         mutableStateOf("Unknown")
     }
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier
-                .fillMaxHeight(0.5f)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Temp in $name = ${state.value} C")
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(),
-            contentAlignment = Alignment.BottomCenter
-        ) {
-            Button(onClick = {
-                getResult(name, state, context)
-            }, modifier = Modifier.padding(20.dp)) {
-                Text(text = "Refresh")
-            }
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Temp in $name = ${state.value} C")
+        Button(onClick = {
+            getResult(name, state, context)
+        },
+            modifier = Modifier.padding(20.dp)) {
+            Text(text = "Refresh")
         }
     }
 }
@@ -72,13 +125,11 @@ private fun getResult(city: String, state: MutableState<String>, context: Contex
     val stringRequest = StringRequest(
         Request.Method.GET,
         url,
-        {
-            response ->
+        { response ->
             val obj = JSONObject(response)
             state.value = obj.getJSONObject("current").getString("temp_c")
         },
-        {
-            error ->
+        { error ->
         }
     )
     queue.add(stringRequest)
