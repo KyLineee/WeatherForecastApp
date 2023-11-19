69c09dacf65c30acec140d6819aae8a1ce753a8b
Ali <alihuseyni523@gmail.com>
2023-11-19 12:07:40 +0300

    Adding my chart

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -2,48 +2,32 @@ package com.example.weatherforecast
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarData
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
@@ -54,7 +38,6 @@ import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -86,7 +69,6 @@ import org.json.JSONArray
 import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.Calendar
-import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
 
@@ -435,7 +417,7 @@ fun getList(list: List<WeatherModel>, currentDay: MutableState<WeatherModel>) {
         ) { i, item ->
             ListItem(item, currentDay)
         }
-        item { chart() }
+        //item { chart(list) }
     }
 }
 
--- app/src/main/java/com/example/weatherforecast/chart.kt
+++ app/src/main/java/com/example/weatherforecast/chart.kt
@@ -1,140 +1,140 @@
 package com.example.weatherforecast
 
-import android.graphics.Paint
+
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.TextMeasurer
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import co.yml.charts.axis.AxisData
 import co.yml.charts.common.model.Point
-import co.yml.charts.ui.linechart.LineChart
-import co.yml.charts.ui.linechart.model.GridLines
-import co.yml.charts.ui.linechart.model.IntersectionPoint
-import co.yml.charts.ui.linechart.model.Line
-import co.yml.charts.ui.linechart.model.LineChartData
-import co.yml.charts.ui.linechart.model.LinePlotData
-import co.yml.charts.ui.linechart.model.LineStyle
-import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
-import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
-import co.yml.charts.ui.linechart.model.ShadowUnderLine
-import org.w3c.dom.Text
-import java.lang.Float.min
+import com.example.weatherforecast.data.WeatherModel
+import java.lang.Math.abs
 import kotlin.random.Random
 
-const val steps = 6
 @Composable
-fun chart(){
-    val pointsList = getPointsList()
-    val max = getMax(pointsList)
-    val min = getMin(pointsList)
-    val xAxisData = AxisData.Builder()
-        .axisStepSize(50.dp)
-        .backgroundColor(Color.Transparent)
-        .steps(pointsList.size - 1)
-        .labelData { i -> i.toString() + 'h'}
-        .labelAndAxisLinePadding(15.dp)
-        .build()
+fun chart(list: List<WeatherModel>) {
+    var listOffset = getPointsList(list)
+    drawPoints(listOffset)
+    drawText(listOffset)
+}
 
-    val yAxisData = AxisData.Builder()
-        .steps(steps)
-        .backgroundColor(Color.Transparent)
-        .labelAndAxisLinePadding(15.dp)
-        .labelData {  i ->
-            val yScale = (max - min) / steps.toFloat()
-            String.format("%.1f", ((i * yScale) + min))
-        }
-        .build()
+@Composable
+fun drawText(pointsList: List<Offset>) {
     val textMeasurer = rememberTextMeasurer()
-    val style = TextStyle(
-        fontSize = 20.sp,
-        color = Color.Black,
-    )
-    val windowSize = LocalConfiguration.current.screenWidthDp
-    val lineChartData = LineChartData(
-        linePlotData = LinePlotData(
-            lines = listOf(
-                Line(
-                    dataPoints = pointsList,
-                    LineStyle(color = Color.Green, width = 5.0f),
-                    IntersectionPoint(
-                        radius = 4.dp,
-                        draw = { offset ->
-                            val xPosition = min(offset.x, windowSize.toFloat() + 660f)
-                            drawCircle(color = Color.Black, radius = 14f, center = offset)
-                            drawText(
-                                textMeasurer = textMeasurer,
-                                text = "11",
-                                style = style,
-                                topLeft = Offset(
-                                    x = xPosition - 30f,
-                                    y = offset.y - 90f,
-                                )
-                            )
-                        },
-                    ),
-                    SelectionHighlightPoint(color = Color.Black, alpha = 0.4f, radius = 5.dp),
-                    ShadowUnderLine(),
-                    SelectionHighlightPopUp()
-                )
-            ),
-        ),
-        xAxisData = xAxisData,
-        yAxisData = yAxisData,
-        gridLines = GridLines(color = Color.Transparent),
-        backgroundColor = Color.Transparent
-    )
-    LineChart(
+    LazyRow(
         modifier = Modifier
             .fillMaxWidth()
-            .height(220.dp),
-        lineChartData = lineChartData
-    )
-}
-fun getPointsList(): List<Point> {
-    val list = ArrayList<Point>()
-    for (i in 0..23){
-        list.add(
-            Point(
-                i.toFloat(),
-                Random.nextInt(-30,30).toFloat()
-            )
-        )
+            .height(200.dp)
+    ) {
+        itemsIndexed(
+            pointsList
+        ) { i, item ->
+            Canvas(
+                modifier = Modifier
+                    .width(36.dp)
+                    .height(200.dp)
+            ) {
+                var y = item.y - 200f
+                if(y < 0) {
+                    y += 2 * abs(y)
+                }
+                else {
+                    y -= 2 * abs(y)
+                }
+                var yText = y.toString()
+                drawText(
+                    textMeasurer = textMeasurer,
+                    text = yText,
+                    topLeft = Offset(0f, item.y - 60f)
+                )
+            }
+        }
     }
-    return list
 }
 
-fun getMax(list: List<Point>): Float{
+@Composable
+fun drawPoints(pointsList: List<Offset>) {
+    Canvas(modifier = Modifier.size(200.dp)) {
+        drawPoints(
+            points = pointsList,
+            strokeWidth = 12f,
+            pointMode = PointMode.Points,
+            color = Color.Blue,
+            cap = StrokeCap.Round
+        )
+        drawPoints(
+            points = pointsList,
+            strokeWidth = 4f,
+            pointMode = PointMode.Polygon,
+            color = Color.Blue,
+            cap = StrokeCap.Round
+        )
+    }
+}
+
+fun getPointsList(list: List<WeatherModel>): List<Offset> {
+    val listOffset = ArrayList<Offset>()
+    var x = 0f
+    var y = 0f
+    for (i in 0 until list.size) {
+        y = list[i].currentTemp.toFloat()
+        if(y < 0) {
+            y += 2 * abs(y)
+        }
+        else {
+            y -= 2 * y
+        }
+        y += 200f
+        listOffset.add(Offset(x, y))
+        x += 100
+    }
+    return listOffset
+}
+
+fun getPointsListExample(): List<Offset> {
+    val listOffset = ArrayList<Offset>()
+    var x = 50f
+    var y = 0f
+    for (i in 0..10) {
+        y = Random.nextInt(-30, 30).toFloat()
+        if(y < 0) {
+            y += 2 * abs(y)
+        }
+        else {
+            y -= 2 * y
+        }
+        y += 200f
+        listOffset.add(Offset(x, y))
+        x += 100
+    }
+    return listOffset
+}
+
+fun getMax(list: List<Point>): Float {
     var max = 0f
-    list.forEach{point ->
-        if(max < point.y) max = point.y
+    list.forEach { point ->
+        if (max < point.y) max = point.y
     }
     return max
 }
 
-fun getMin(list: List<Point>): Float{
+fun getMin(list: List<Point>): Float {
     var min = 1000f
-    list.forEach{point ->
-        if(min > point.y) min = point.y
+    list.forEach { point ->
+        if (min > point.y) min = point.y
     }
     return min
 }
